a18ee8da01dff7280609cb15dce46484fc3c4b98
Vinicius Bessa <vinicius.b.silva3@gmail.com>
2023-09-27 20:45:43 -0300

    feat: Prisma models and enums were added

--- server.ts
+++ server.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import app from './app';
+
+const port = process.env.PORT || 5000;
+const url = process.env.URL || 'http://ocalhost';
+
+app.listen(port, () => console.log(`The server is running on: ${url}:${port}`));
